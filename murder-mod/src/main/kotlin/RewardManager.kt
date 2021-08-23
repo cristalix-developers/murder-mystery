64388e13d08bc92403cae61a9e0a14b92acc6b96
reidjJava <sp1.ke@mail.ru>
2021-08-23 14:30:24 +0500

    Ежедневная награда отображается на экране

--- murder-mod/src/main/kotlin/RewardManager.kt
+++ murder-mod/src/main/kotlin/RewardManager.kt
@@ -6,18 +6,17 @@ import ru.cristalix.uiengine.UIEngine
 class RewardManager {
 
     init {
-        lateinit var week: MutableList<Day>
+        val week = mutableListOf<Day>()
         var currentDay: Int?
 
         UIEngine.registerHandler(PluginMessage::class.java) {
             if (channel == "murder:weekly-reward") {
                 currentDay = data.readInt()
                 repeat(7) {
-                    week.add(Day(it + 1, ItemTools.read(data), NetUtil.readUtf8(data), it + 1 < currentDay!!))
+                    week.add(Day(it + 1, ItemTools.read(data), NetUtil.readUtf8(data), it + 1 > currentDay!!))
                 }
                 UIEngine.clientApi.minecraft().setIngameNotInFocus()
             }
         }
     }
-
 }
