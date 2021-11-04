26a8bfbb41c585cfb922e7c369dacad117e468b6
reidjJava <sp1.ke@mail.ru>
2021-11-04 20:55:38 +0300

    Update WeekRewards.kt

--- murder-commons/src/main/kotlin/me/func/commons/content/WeekRewards.kt
+++ murder-commons/src/main/kotlin/me/func/commons/content/WeekRewards.kt
@@ -1,12 +1,14 @@
 package me.func.commons.content
 
 import dev.implario.bukkit.item.item
+import me.func.commons.donate.DonatePosition
 import me.func.commons.donate.impl.*
 import me.func.commons.gold
 import me.func.commons.user.User
 import org.bukkit.Material
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.inventory.ItemStack
+import ru.cristalix.core.formatting.Formatting
 
 enum class WeekRewards(val title: String, val icon: ItemStack, val give: (User) -> Any) {
     ONE("§e32 монеты", gold, { it.giveMoney(32) }),
@@ -14,22 +16,22 @@ enum class WeekRewards(val title: String, val icon: ItemStack, val give: (User) 
     THREE(
         Corpse.G2.getRare().with(Corpse.G2.getTitle()),
         Corpse.G2.getIcon(),
-        { Corpse.G2.give(it) }
+        { me.func.commons.content.WeekRewards.Companion.giveWithDuplicate(it, Corpse.G2, 64) }
     ),
     FOUR(
         StepParticle.FALLING_DUST.getRare().with(StepParticle.FALLING_DUST.getTitle()),
         StepParticle.FALLING_DUST.getIcon(),
-        { StepParticle.FALLING_DUST.give(it) }
+        { me.func.commons.content.WeekRewards.Companion.giveWithDuplicate(it, StepParticle.FALLING_DUST, 128) }
     ),
     FIVE(
         NameTag.NINJA.getRare().with(NameTag.NINJA.getTitle()),
         NameTag.NINJA.getIcon(),
-        { NameTag.NINJA.give(it) }
+        { me.func.commons.content.WeekRewards.Companion.giveWithDuplicate(it, NameTag.NINJA, 256) }
     ),
     SIX(
         Mask.ONI.getRare().with(Mask.ONI.getTitle()),
         Mask.ONI.getIcon(),
-        { Mask.ONI.give(it) }
+        { me.func.commons.content.WeekRewards.Companion.giveWithDuplicate(it, Mask.ONI, 512) }
     ),
     SEVEN(
         "§b5 Лутбоксов",
@@ -39,4 +41,16 @@ enum class WeekRewards(val title: String, val icon: ItemStack, val give: (User) 
             nbt("other", "enderchest1")
         }.build(), { repeat(5) { _ -> it.stat.lootbox++ } }
     )
+    ;
+
+    companion object {
+        fun giveWithDuplicate(user: User, donate: DonatePosition, reward: Int) {
+            if (user.stat.donate.contains(donate)) {
+                user.stat.money += reward
+                user.player!!.sendMessage(Formatting.fine("§aДубликат! §fЗаменен на §e$reward монет§f."))
+            } else {
+                donate.give(user)
+            }
+        }
+    }
 }
