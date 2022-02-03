e6d2c7c953e480347de1e55f5ea3505206c23f2a
Kamillaova <54859825+Kamillaova@users.noreply.github.com>
2022-02-03 13:03:21 +0300

    -Xlambdas=indy

---
+++ murder-game/src/main/kotlin/me/func/murder/dbd/DbdTimer.kt
@@ -0,0 +1,30 @@
+package me.func.murder.dbd
+
+import me.func.murder.MurderGame
+import me.func.murder.getUser
+import me.func.murder.mod.ModTransfer
+import org.bukkit.scheduler.BukkitRunnable
+
+class DbdTimer(private val game: MurderGame) : BukkitRunnable() {
+    var time = 0
+    var playersBefore = 0
+
+    override fun run() {
+        // Обновление шкалы онлайна
+        val players = game.players
+        if (playersBefore != players.size) {
+            game.context.after(10) {
+                players.forEach {
+                    ModTransfer()
+                        .integer(game.slots)
+                        .integer(players.size)
+                        .boolean(true)
+                        .send("update-online", game.userManager.getUser(it))
+                }
+            }
+            playersBefore = players.size
+        }
+
+        time = game.activeDbdStatus.now(time, game) + 1
+    }
+}
