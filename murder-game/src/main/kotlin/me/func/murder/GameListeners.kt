6c6eaca1f1731ce887d23f63e7366d0e9b1ccbc3
Kamillaova <54859825+Kamillaova@users.noreply.github.com>
2022-03-07 05:12:35 +0300

    ok.

--- murder-game/src/main/kotlin/me/func/murder/GameListeners.kt
+++ murder-game/src/main/kotlin/me/func/murder/GameListeners.kt
@@ -109,12 +109,6 @@ class GameListeners(private val game: MurderGame, dbd: Boolean) {
                 setupInteractListeners()
                 setupInventoryListeners()
                 setupMapDecorationListeners()
-
-                // reset bow (@funcid я не знаю как ведет себя лук, если ты не детектив, и есть ли он вообще))) )
-                context.on<EntityShootBowEvent> {
-                    val user = game.userManager.getUser(entity as? Player ?: return@on)
-                    if (user.role == Role.DETECTIVE) Role.DETECTIVE.start(user, game)
-                }
             }
             setupGlobalListeners()
         }
